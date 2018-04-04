225011254ae89637ebe06c834f64c46dc3106808
muhammadsahrul <35890836+muhammadsahrul@users.noreply.github.com>
2018-04-05 05:14:13 +0800

    Membuat Class Aksi dan Method Main

---
+++ src/tugas01/ManusiaAksi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas01;
+
+/**
+ *
+ * @author Acer E5 473G
+ */
+public class ManusiaAksi {
+    public static void main(String[] args) {
+        
+    }
+}
